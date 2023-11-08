832dd2b3c9f392b15ad66f7d343dc8ca990f8ae9
jonathan <jonathhu@stud.ntnu.no>
2023-11-08 12:02:22 +0100

    Cleanup and constructor

---
+++ Oving_11/src/Property.java
@@ -0,0 +1,21 @@
+public class Property {
+
+  private int municipalityNumber;
+  private String municipalityName;
+  private int lotNumber;
+  private int sectionNumber;
+  private String name;
+  private float area;
+  private String nameOfOwner;
+
+  public Property(int municipalityNumber, String municipalityName, int lotNumber, int sectionNumber, String name, float area, String nameOfOwner) {
+    this.municipalityNumber = municipalityNumber;
+    this.municipalityName = municipalityName;
+    this.lotNumber = lotNumber;
+    this.sectionNumber = sectionNumber;
+    this.name = name;
+    this.area = area;
+    this.nameOfOwner = nameOfOwner;
+
+  }
+}
