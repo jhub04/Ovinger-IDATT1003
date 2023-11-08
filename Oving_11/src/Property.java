b0bd2c1847592e1581c51004970197f112748b40
jonathan <jonathhu@stud.ntnu.no>
2023-11-08 13:31:58 +0100

    Javadoc

--- Oving_11/src/Property.java
+++ Oving_11/src/Property.java
@@ -1,3 +1,10 @@
+/**
+ * Class for properties
+ *
+ * @author Jonathan Hübertz
+ * @version 1.0
+ * @since 2023-11-08
+ */
 public class Property {
 
   private final int municipalityNumber;
@@ -8,6 +15,17 @@ public class Property {
   private final float area;
   private String nameOfOwner;
 
+  /**
+   * Constructor for properties with name
+   *
+   * @param municipalityNumber the municipality number
+   * @param municipalityName   the municipality name
+   * @param lotNumber          the lot number
+   * @param sectionNumber      the section number
+   * @param name               the name
+   * @param area               the area
+   * @param nameOfOwner        the name of the owner
+   */
   public Property(int municipalityNumber, String municipalityName, int lotNumber, int sectionNumber,
       String name, float area, String nameOfOwner) {
     this.municipalityNumber = municipalityNumber;
@@ -20,6 +38,16 @@ public class Property {
 
   }
 
+  /**
+   * Constructor for properties without name
+   *
+   * @param municipalityNumber the municipality number
+   * @param municipalityName   the municipality name
+   * @param lotNumber          the lot number
+   * @param sectionNumber      the section number
+   * @param area               the area
+   * @param nameOfOwner        the name of the owner
+   */
   public Property(int municipalityNumber, String municipalityName, int lotNumber, int sectionNumber,
       float area, String nameOfOwner) {
     this.municipalityNumber = municipalityNumber;
@@ -31,33 +59,74 @@ public class Property {
     this.nameOfOwner = nameOfOwner;
   }
 
+  /**
+   * Returns the municipality number
+   *
+   * @return municipality number
+   */
   public int getMunicipalityNumber() {
     return municipalityNumber;
   }
 
+  /**
+   * Returns the municipality name
+   *
+   * @return municipality name
+   */
   public String getMunicipalityName() {
     return municipalityName;
   }
 
+  /**
+   * Returns the lot number
+   *
+   * @return lot number
+   */
   public int getLotNumber() {
     return lotNumber;
   }
 
+  /**
+   * Returns the section number
+   *
+   * @return section number
+   */
   public int getSectionNumber() {
     return sectionNumber;
   }
 
+  /**
+   * Returns the name
+   *
+   * @return name
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Returns the area
+   *
+   * @return area
+   */
   public float getArea() {
     return area;
   }
 
+  /**
+   * Returns the name of the owner
+   *
+   * @return name of the owner
+   */
   public String getNameOfOwner() {
     return nameOfOwner;
   }
+
+  /**
+   * Sets the name of the owner
+   *
+   * @param nameOfOwner the name of the owner
+   */
 
   public void setNameOfOwner(String nameOfOwner) {
     this.nameOfOwner = nameOfOwner;
