272f9995d3be60fdb921a1e64a436f891eb65ff9
jonathan <jonathhu@stud.ntnu.no>
2023-11-08 12:21:08 +0100

    Final variables and second constructor

--- Oving_11/src/Property.java
+++ Oving_11/src/Property.java
@@ -1,14 +1,15 @@
 public class Property {
 
-  private int municipalityNumber;
-  private String municipalityName;
-  private int lotNumber;
-  private int sectionNumber;
-  private String name;
-  private float area;
+  private final int municipalityNumber;
+  private final String municipalityName;
+  private final int lotNumber;
+  private final int sectionNumber;
+  private final String name;
+  private final float area;
   private String nameOfOwner;
 
-  public Property(int municipalityNumber, String municipalityName, int lotNumber, int sectionNumber, String name, float area, String nameOfOwner) {
+  public Property(int municipalityNumber, String municipalityName, int lotNumber, int sectionNumber,
+      String name, float area, String nameOfOwner) {
     this.municipalityNumber = municipalityNumber;
     this.municipalityName = municipalityName;
     this.lotNumber = lotNumber;
@@ -17,5 +18,15 @@ public class Property {
     this.area = area;
     this.nameOfOwner = nameOfOwner;
 
+  }
+
+  public Property(int municipalityNumber, String municipalityName, int lotNumber, int sectionNumber, float area, String nameOfOwner) {
+    this.municipalityNumber = municipalityNumber;
+    this.municipalityName = municipalityName;
+    this.lotNumber = lotNumber;
+    this.sectionNumber = sectionNumber;
+    this.name = "";
+    this.area = area;
+    this.nameOfOwner = nameOfOwner;
   }
 }
