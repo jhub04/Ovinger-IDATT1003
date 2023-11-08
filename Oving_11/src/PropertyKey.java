b8ca208e41bf7a8ebca34db2414fecfdc6f445ad
jonathan <jonathhu@stud.ntnu.no>
2023-11-08 13:37:45 +0100

    Javadoc

--- Oving_11/src/PropertyKey.java
+++ Oving_11/src/PropertyKey.java
@@ -1,10 +1,25 @@
 import java.util.Objects;
 
+/**
+ * Represents a property key. This class acts as the property key for the property register.
+ *
+ * @author Jonathan Hübertz
+ * @version 1.0
+ * @since 2023-11-08
+ */
 public class PropertyKey {
 
   private final int municipalityNumber;
   private final int lotNumber;
   private final int sectionNumber;
+
+  /**
+   * Constructor for property key
+   *
+   * @param municipalityNumber the municipality number
+   * @param lotNumber the lot number
+   * @param sectionNumber the section number
+   */
 
   public PropertyKey(int municipalityNumber, int lotNumber, int sectionNumber) {
     this.municipalityNumber = municipalityNumber;
@@ -12,18 +27,39 @@ public class PropertyKey {
     this.sectionNumber = sectionNumber;
   }
 
+  /**
+   * Getter for municipality number
+   *
+   * @return municipality number
+   */
   public int getMunicipalityNumber() {
     return municipalityNumber;
   }
 
+  /**
+   * Getter for lot number
+   *
+   * @return lot number
+   */
   public int getLotNumber() {
     return lotNumber;
   }
 
+  /**
+   * Getter for section number
+   *
+   * @return section number
+   */
   public int getSectionNumber() {
     return sectionNumber;
   }
 
+  /**
+   * Checks if two property keys are equal
+   *
+   * @param o the object to compare to
+   * @return true if the property keys are equal, false if not
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -38,11 +74,19 @@ public class PropertyKey {
         sectionNumber == that.sectionNumber;
   }
 
+  /**
+   * Generates a hash code for the property key
+   * @return the hash code
+   */
   @Override
   public int hashCode() {
     return Objects.hash(municipalityNumber, lotNumber, sectionNumber);
   }
 
+  /**
+   * Provides a string representation of a property key
+   * @return a string representation of a property key
+   */
   @Override
   public String toString() {
     return this.municipalityNumber + "-" + this.lotNumber + "/" + this.sectionNumber;
