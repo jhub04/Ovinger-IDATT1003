51aedbd3fc64eb1638ff0687c9f1cb992b103ea6
jonathan <jonathhu@stud.ntnu.no>
2023-11-08 13:11:16 +0100

    Made a class for the key to the property

---
+++ Oving_11/src/PropertyKey.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class PropertyKey {
+  private final int municipalityNumber;
+  private final int lotNumber;
+  private final int sectionNumber;
+
+  public PropertyKey(int municipalityNumber, int lotNumber, int sectionNumber) {
+    this.municipalityNumber = municipalityNumber;
+    this.lotNumber = lotNumber;
+    this.sectionNumber = sectionNumber;
+  }
+
+  public int getMunicipalityNumber() {
+    return municipalityNumber;
+  }
+
+  public int getLotNumber() {
+    return lotNumber;
+  }
+
+  public int getSectionNumber() {
+    return sectionNumber;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof PropertyKey)) return false;
+    PropertyKey that = (PropertyKey) o;
+    return municipalityNumber == that.municipalityNumber &&
+        lotNumber == that.lotNumber &&
+        sectionNumber == that.sectionNumber;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(municipalityNumber, lotNumber, sectionNumber);
+  }
+}
